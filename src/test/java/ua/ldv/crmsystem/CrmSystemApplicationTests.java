7e24bfc91f14e26dcf0dda0c16037b1250c8705c
nugos <ldv@ldv.com.ua>
2023-11-20 21:00:34 +0200

    git init

---
+++ src/test/java/ua/ldv/crmsystem/CrmSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.ldv.crmsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
